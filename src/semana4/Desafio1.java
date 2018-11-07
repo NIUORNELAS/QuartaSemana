68ce24c1609da51a3890235a7e48134c388b25ce
NIUORNELAS <noilsonornelas@hotmail.com>
2018-11-07 21:10:51 -0200

    estou quase lá

--- src/semana4/Desafio1.java
+++ src/semana4/Desafio1.java
@@ -26,7 +26,8 @@ estoque.produto[0] = new Produto();
     	estoque.produto[0].setNome ("Camiseta");
     	estoque.produto[0].setTamanho ("M");
     	estoque.produto[0].setCor ("Vermelha");
-    	estoque.produto[0].setMarca ("Element");
+estoque.produto[0].setMarca ("Element");
+    	estoque.produto[0].setQuantidade (5);
     	
     	System.out.println (estoque.produto[0].NIU());
     }
@@ -36,7 +37,9 @@ private String nome;
 private String tamanho;
   private String cor;
 private String marca;
+private int quantidade;
 
+// get e set 
 public String getNome() {
 	return this.nome;}
 public void setNome(String nome) {
@@ -56,22 +59,27 @@ public String getMarca() {
 return this.marca;}
 public void setMarca(String marca) {
 	this.marca=marca;} 
+public int getQuantidade() {
+    return this.quantidade;}
+public void setQuantidade(int quantidade) { 
+	this.quantidade=quantidade;}
 
-    String NIU() 
+	String NIU() 
 {
-	return this.getNome() + " " + this.getCor() + ", " + "Tamanho" +" "+ this.getTamanho() +", "+ "Marca" +" "+this.getMarca();
+	return this.getNome() + " " + this.getCor() + ", " + "Tamanho" +" "+ this.getTamanho() +", "+ "Marca" +" "+this.getMarca() +", "+ "Quantidade em estoque= " + this.getQuantidade();
 }
-    	}
+}
     
 class Estoque {
-	private int quantidade= 1;
-private int id;
-Produto[] produto=new Produto[(this.getQuantidade())]; 
-	    int getQuantidade() {
-	return this.quantidade;}
-public void setQuantidade(int quantidade) {
-	this.quantidade=quantidade;}
-	
+private int q=1;
+	private int id;
+Produto[] produto=new Produto[(this.getQ())]; 
+
+public int getQ() {
+	return this.q;}
+public void setQ(int q) {
+	this.q=q;}
+
 	public int getId() {
 	return this.id;}
 	public void setId(int id) {	
